962c463681bb237c60d7f41c7c653bb07991777c
Nathaliemug <42577410+Nathaliemug@users.noreply.github.com>
2019-07-23 09:42:47 +0200

    CORRECTION DE LA CONNEXIONDB

--- library/connexiodb.php
+++ library/connexiodb.php
@@ -22,7 +22,45 @@ if($reponse->rowCount()== 0)
                 $first_name= $row["first_name"];
                 $last_name= $row["last_name"]; 
             }
-           $reponse = [
+            <?php
+            $host='localhost';
+            $dbname='sirius_school';
+            $charset='utf8';
+            $dbuser='root';
+            $dbpwd='';
+            
+            //DEFINIE L'ERREUR
+            define('DEBUG',true);
+            
+            
+            // SI IL Y A PAS D'ERREUR ALORS ERREUR REPORT ZERO
+            if(!DEBUG){
+            
+                error_reporting(0);
+            }
+            // ESSAYE DE TE CONNECTER SUR LA BASE DES DONNEES
+            try {
+                $bdd= new PDO("mysql:host=$host;dbname=$dbname;charset=$charset",$dbuser,$dbpwd);
+            } 
+            
+            //SI TU N'Y ARRIVE PAS , TROUVE MOI L'ERREUR
+            catch (exception $e) {
+                
+                //SI IL Y A L'ERREUR
+                if(DEBUG){
+            
+                    //STOP ET AFFICHE MOI L'ERREUR 
+                    
+                    die('error:'. $e->getMessage());
+                }
+                else{
+                    
+                    //SI NON AFFICHER MOI CONNEXION ERROR 
+                     die('connexion error');
+                }
+            }
+            ?>
+               $reponse = [
             "error"         => false,
             "error_message" => "",
             "data"          => [$first_name,$last_name]
