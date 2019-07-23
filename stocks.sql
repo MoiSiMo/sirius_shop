f209031080a95d7e693cd1fd8844b3976d772be2
MoiSiMo <51946449+MoiSiMo@users.noreply.github.com>
2019-07-23 09:19:16 +0200

    Add files via upload

---
+++ stocks.sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le :  mar. 23 juil. 2019 à 09:18
+-- Version du serveur :  10.3.16-MariaDB
+-- Version de PHP :  7.3.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `stocks`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_achatsproduits`
+--
+-- Erreur de lecture de structure pour la table stocks.t_achatsproduits : #1932 - Table 'stocks.t_achatsproduits' doesn't exist in engine
+-- Erreur de lecture des données pour la table stocks.t_achatsproduits : #1064 - Erreur de syntaxe près de 'FROM `stocks`.`t_achatsproduits`' à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_clients`
+--
+-- Erreur de lecture de structure pour la table stocks.t_clients : #1932 - Table 'stocks.t_clients' doesn't exist in engine
+-- Erreur de lecture des données pour la table stocks.t_clients : #1064 - Erreur de syntaxe près de 'FROM `stocks`.`t_clients`' à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_factures`
+--
+-- Erreur de lecture de structure pour la table stocks.t_factures : #1932 - Table 'stocks.t_factures' doesn't exist in engine
+-- Erreur de lecture des données pour la table stocks.t_factures : #1064 - Erreur de syntaxe près de 'FROM `stocks`.`t_factures`' à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_fournisseurs`
+--
+-- Erreur de lecture de structure pour la table stocks.t_fournisseurs : #1932 - Table 'stocks.t_fournisseurs' doesn't exist in engine
+-- Erreur de lecture des données pour la table stocks.t_fournisseurs : #1064 - Erreur de syntaxe près de 'FROM `stocks`.`t_fournisseurs`' à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_produits`
+--
+-- Erreur de lecture de structure pour la table stocks.t_produits : #1932 - Table 'stocks.t_produits' doesn't exist in engine
+-- Erreur de lecture des données pour la table stocks.t_produits : #1064 - Erreur de syntaxe près de 'FROM `stocks`.`t_produits`' à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_ventesproduits`
+--
+-- Erreur de lecture de structure pour la table stocks.t_ventesproduits : #1932 - Table 'stocks.t_ventesproduits' doesn't exist in engine
+-- Erreur de lecture des données pour la table stocks.t_ventesproduits : #1064 - Erreur de syntaxe près de 'FROM `stocks`.`t_ventesproduits`' à la ligne 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
