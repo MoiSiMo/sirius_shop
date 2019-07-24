93eb46bd10f77ec15ee517bf636d200066d01767
MoiSiMo <51946449+MoiSiMo@users.noreply.github.com>
2019-07-24 14:13:10 +0200

    Update ajoproduits.php

--- produits/ajoproduits.php
+++ produits/ajoproduits.php
@@ -12,43 +12,48 @@ $response = [
     "data"          => NULL
 ];
 //on vérifie si les données existe
-if(isset($_REQUEST["NumFour"]) && isset($_REQUEST["NomProd"])&& isset($_REQUEST["NumSCat"]))
+if(isset($_REQUEST["NumFour"]) && isset($_REQUEST["NomProd"])&& isset($_REQUEST["NumSCat"])&& isset($_REQUEST["QtProd"])&& isset($_REQUEST["QtMinProd"]))
 
 {
-// on récupere les données
-    $NomProd = $_REQUEST["NomProd"];
-    $NumFour = $_REQUEST["NumFour"];
-    $NumSCat = $_REQUEST["NumSCat"];
-// on insere les produits dans la table  
+        // on récupere les données
+            $NumFour = $_REQUEST["NumFour"];
+            $NomProd = $_REQUEST["NomProd"];
+            $NumSCat = $_REQUEST["NumSCat"];
+            $QtProd = $_REQUEST["QtProd"];
+            $QtMinProd = $_REQUEST["QtMinProd"];
+        // on insere les produits dans la table  
 
-$sql_Inserer_produits = " INSERT INTO t_produits 
-(NumFour, NomProd, NumSCat) VALUES (?,?,?)";
+        $sql_Inserer_produits="INSERT INTO t_produits (NumFour, NomProd, NumSCat, QtProd, QtMinProd) VALUES (?,?,?,?,?)";
 
-// on prepare la requete    
+        // on prepare la requete    
 
-$request1 = $bdd->prepare($sql_Inserer_produits);
+        $request1 = $bdd->prepare($sql_Inserer_produits);
 
-    $request1->bindParam(1, $NumFour, PDO::PARAM_INT);
-    $request1->bindParam(2, $NomProd, PDO::PARAM_STR);
-    $request1->bindParam(3, $NumSCat, PDO::PARAM_INT);
-  //on execute la requete  
-    $request1->execute();
+            $request1->bindParam(1, $NumFour, PDO::PARAM_INT);
+            $request1->bindParam(2, $NomProd, PDO::PARAM_STR);
+            $request1->bindParam(3, $NumSCat, PDO::PARAM_INT);
+            $request1->bindParam(4, $QtProd, PDO::PARAM_INT);
+            $request1->bindParam(5, $QtMinProd, PDO::PARAM_INT);
+        //on execute la requete  
+            $request1->execute();
 
-    $data = "ok";
-    $response["data"] = $data;
-    $response["error_message"] = "";
-    $response["error"] = false;
-    $response["msg"]="données enregistré";   
+            $data = "ok";
+            $response["data"] = $data;
+            $response["error_message"] = "";
+            $response["error"] = false;
+            $response["msg"]="données enregistré";   
 
-    //on encode le php par json 
-    $json=json_encode($response);
+            //on encode le php par json 
+            $json=json_encode($response);
 
-    // on  affiche le json    
-    echo $json;
+            // on  affiche le json    
+            echo $json;
 
-    die();
+            die();
 
-    }  else {
-        $response["error_message"] = "pas de données";
-    }
+}  
+else 
+{
+    $response["error_message"] = "pas de données";
+}
 ?>
