01ebacb41278dc2c30f8f06c17cbc1ce1adc06fb
MoiSiMo <51946449+MoiSiMo@users.noreply.github.com>
2019-07-24 12:20:56 +0200

    Update supproduits.php

--- produits/supproduits.php
+++ produits/supproduits.php
@@ -14,19 +14,25 @@ $response = [
     "data"          => NULL,
 ];
 
-if(!isset($_REQUEST["NamProd"]) || empty($_REQUEST["NamProd"]) || !is_numeric($_REQUEST["NamProd"]))
+if(!isset($_REQUEST["NumProd"]) || empty($_REQUEST["NumProd"]) || !is_numeric($_REQUEST["NumProd"]))
 {
     $response["error_message"] = "Erreur paramètre";
     echo json_encode($response);
     die();
 }
 
-$NamProd = $_REQUEST["NamProd"];
+$NumProd = $_REQUEST["NumProd"];
 
 /* Requête : on récupère le premier résultat dans studebts*/
-$sth = $bdd->prepare('DELETE NumProd, NomProd , NumSCat, NumFour, NumSCat FROM t_produits WHERE NumProd = :NumProd');
-$sth->bindValue(":NamProd", $NamProd, PDO::PARAM_INT);
+$sth = $bdd->prepare('DELETE NumProd, NumFour, NomProd , NumSCat, QtProd, QtMinProd FROM t_produits WHERE NumProd = :NumProd');
+$sth->bindValue(":NumProd", $NumProd, PDO::PARAM_INT);
+$sth->bindValue(":NumFour", $NumFour, PDO::PARAM_INT);
+$sth->bindValue(":NomProd", $NomProd, PDO::PARAM_STR);
+$sth->bindValue(":NumSCat", $NumSCat, PDO::PARAM_INT);
+$sth->bindValue(":QtProd", $QtProd, PDO::PARAM_INT);
+$sth->bindValue(":QtMinProd", $QtMinProd, PDO::PARAM_INT);
 $result = $sth->execute();
+
 if($result)
 {
     $data = "ok";
