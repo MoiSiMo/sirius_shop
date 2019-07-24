9ffd07c80d73026546ce7eb0473aad46fb464841
Nathaliemug <42577410+Nathaliemug@users.noreply.github.com>
2019-07-24 16:16:40 +0200

    CATEGORIEAJOUT FINI&&PRODUITSAJOUT &&correction library/connexion sur if (debug ,true)
    donc j'ai changé le false en true pour afficher les erreurs

--- produits/ajoproduits.php
+++ produits/ajoproduits.php
@@ -1,59 +1,67 @@
-<?php
-require "../library/cors.php";
-require "../library/connexiondb.php";
-cors();
+    <?php
 
-header('Content-Type: application/json');
+    require "../library/cors.php";
+    require "../library/connexiondb.php";
+    cors();
+    header('Content-Type: application/json');
+    /* Réponse par défaut*/
+    $response = [
+        "error"         => true,
+        "error_message" => "unknown Error",
+        "data"          => ""
+    ];
+    //on vérifie si les données existe
+    if(isset($_REQUEST["NumFour"]) && isset($_REQUEST["NomProd"])&& 
+    isset($_REQUEST["NumSCat"])&& isset($_REQUEST["QtProd"])&& 
+    isset($_REQUEST["QtMinProd"]))
+    {
+            // on récupere les données
+                $NumFour = $_REQUEST["NumFour"];
+                $NomProd = $_REQUEST["NomProd"];
+                $NumSCat = $_REQUEST["NumSCat"];
+                $QtProd = $_REQUEST["QtProd"];
+                $QtMinProd = $_REQUEST["QtMinProd"];
+            // on insere les produits dans la table 
+            
+            //var_dump($_REQUEST);
 
-/* Réponse par défaut*/
-$response = [
-    "error"         => true,
-    "error_message" => "unknown Error",
-    "data"          => NULL
-];
-//on vérifie si les données existe
-if(isset($_REQUEST["NumFour"]) && isset($_REQUEST["NomProd"])&& isset($_REQUEST["NumSCat"])&& isset($_REQUEST["QtProd"])&& isset($_REQUEST["QtMinProd"]))
+        
 
-{
-        // on récupere les données
-            $NumFour = $_REQUEST["NumFour"];
-            $NomProd = $_REQUEST["NomProd"];
-            $NumSCat = $_REQUEST["NumSCat"];
-            $QtProd = $_REQUEST["QtProd"];
-            $QtMinProd = $_REQUEST["QtMinProd"];
-        // on insere les produits dans la table  
+                try{
+                    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                    $sql_Inserer_produits="INSERT INTO t_produits (NumFour, NomProd, NumSCat, QtProd, QtMinProd) VALUES (?,?,?,?,?)";
 
-        $sql_Inserer_produits="INSERT INTO t_produits (NumFour, NomProd, NumSCat, QtProd, QtMinProd) VALUES (?,?,?,?,?)";
+                    // on prepare la requete    
+            
+                    $request1 = $bdd->prepare($sql_Inserer_produits);
+            
+                        $request1->bindParam(1, $NumFour, PDO::PARAM_INT);
+                        $request1->bindParam(2, $NomProd, PDO::PARAM_STR);
+                        $request1->bindParam(3, $NumSCat, PDO::PARAM_INT);
+                        $request1->bindParam(4 , $QtProd, PDO::PARAM_INT);
+                        $request1->bindParam(5, $QtMinProd, PDO::PARAM_INT);
+                    //on execute la requete  
+                        $request1->execute();
 
-        // on prepare la requete    
+                    $data = "ok";
+                    $response["data"] = $data;
+                    $response["error_message"] = "";
+                    $response["error"] = false;
+                    $response["msg"]="données enregistré"; 
+                }
+                catch(Exception $e){
+                    $response["error_message"] = "erreur de données";
+                    $response["error"] = true;
+                }
+                            
 
-        $request1 = $bdd->prepare($sql_Inserer_produits);
+                
+    }  
+    else 
+    {
+        $response["error_message"] = "pas de parametre";
+    }
+    $json=json_encode($response);
 
-            $request1->bindParam(1, $NumFour, PDO::PARAM_INT);
-            $request1->bindParam(2, $NomProd, PDO::PARAM_STR);
-            $request1->bindParam(3, $NumSCat, PDO::PARAM_INT);
-            $request1->bindParam(4, $QtProd, PDO::PARAM_INT);
-            $request1->bindParam(5, $QtMinProd, PDO::PARAM_INT);
-        //on execute la requete  
-            $request1->execute();
-
-            $data = "ok";
-            $response["data"] = $data;
-            $response["error_message"] = "";
-            $response["error"] = false;
-            $response["msg"]="données enregistré";   
-
-            //on encode le php par json 
-            $json=json_encode($response);
-
-            // on  affiche le json    
-            echo $json;
-
-            die();
-
-}  
-else 
-{
-    $response["error_message"] = "pas de données";
-}
-?>
+                // on  affiche le json    
+                echo $json;
