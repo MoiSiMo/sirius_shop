1e97c4c186d65c3be8f0c46edea11a3832b7d2a9
MoiSiMo <51946449+MoiSiMo@users.noreply.github.com>
2019-07-25 06:15:58 +0200

    Add files via upload

--- stocks.sql
+++ stocks.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le :  mer. 24 juil. 2019 à 10:24
--- Version du serveur :  10.3.16-MariaDB
--- Version de PHP :  7.3.6
+-- Généré le :  jeu. 25 juil. 2019 à 06:15
+-- Version du serveur :  10.1.38-MariaDB
+-- Version de PHP :  7.3.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -34,6 +34,16 @@ CREATE TABLE `t_achatsproduits` (
   `QteAch` int(11) NOT NULL,
   `PrixUnitAch` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `t_achatsproduits`
+--
+
+INSERT INTO `t_achatsproduits` (`NumAchProd`, `NumProd`, `QteAch`, `PrixUnitAch`) VALUES
+(1, 5, 10, 80),
+(2, 2, 5, 100),
+(3, 4, 15, 120),
+(4, 6, 20, 130);
 
 -- --------------------------------------------------------
 
@@ -68,13 +78,25 @@ INSERT INTO `t_categories` (`NumCat`, `NomCat`) VALUES
 CREATE TABLE `t_clients` (
   `NumCli` int(11) NOT NULL,
   `NomCli` varchar(50) NOT NULL,
-  `PreCli` varchar(50) NOT NULL,
   `AdrCli` varchar(100) NOT NULL,
   `VilleCli` varchar(50) NOT NULL,
-  `CdePosCli` int(11) NOT NULL,
+  `CdePosCli` varchar(50) NOT NULL,
   `TelFixCli` varchar(50) NOT NULL,
-  `TelPorCli` varchar(50) NOT NULL
+  `TelPorCli` varchar(50) NOT NULL,
+  `NumTVA` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `t_clients`
+--
+
+INSERT INTO `t_clients` (`NumCli`, `NomCli`, `AdrCli`, `VilleCli`, `CdePosCli`, `TelFixCli`, `TelPorCli`, `NumTVA`) VALUES
+(1, 'Spitaels Rene', 'Rue de Thys 37', 'Marche-en-Famenne', '6900', '084214209', '', ''),
+(2, 'Quisquater Muriel', 'Rue de Pondire 24A\r\n', 'Ciney', '5590', ' 083 21 77 75', '0478 97 67 55', ''),
+(3, 'Lepage Stephane', 'Route de Cortil-Wodon 33', 'Eghezée', '5310', '081512138', '', ''),
+(4, 'Eraly Marc', 'Berkenboslaan 4\r\n', 'Tremelo', '3120', '', '0496869645', ''),
+(5, 'Jean Voets, Dhr.', 'Baversstraat 41', 'Tongres', '3700', '012230583', '', ''),
+(6, 'Lion Laurence', 'Rue de Leuze 31', 'Beloeil', '7972', ' 069 55 33 58', '0498 57 04 21', '');
 
 -- --------------------------------------------------------
 
@@ -88,6 +110,16 @@ CREATE TABLE `t_factures` (
   `NumFact` int(11) NOT NULL,
   `DateFact` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `t_factures`
+--
+
+INSERT INTO `t_factures` (`NumListFact`, `NumCli`, `NumFact`, `DateFact`) VALUES
+(1, 5, 1907190001, '2019-07-19'),
+(2, 4, 1907010002, '2019-07-01'),
+(3, 6, 1907150003, '2019-07-15'),
+(4, 3, 190710004, '2019-07-10');
 
 -- --------------------------------------------------------
 
@@ -128,30 +160,28 @@ CREATE TABLE `t_produits` (
   `NumProd` int(11) NOT NULL,
   `NumFour` int(11) NOT NULL,
   `NomProd` varchar(50) NOT NULL,
-  `NumSCat` int(11) NOT NULL,
-  `QtProd` int(11) NOT NULL,
-  `QtMinProd` int(11) NOT NULL
+  `NumSCat` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `t_produits`
 --
 
-INSERT INTO `t_produits` (`NumProd`, `NumFour`, `NomProd`, `NumSCat`, `QtProd`, `QtMinProd`) VALUES
-(1, 1, 'Zalman i3', 1, 5, 2),
-(2, 2, 'Cooler Master MasterBox Q300L', 1, 10, 2),
-(3, 1, 'Zalman i3', 1, 20, 2),
-(4, 2, 'Cooler Master MasterBox Q300L', 1, 11, 2),
-(5, 2, 'Cooler Master MasterBox E500L Argent', 1, 5, 2),
-(6, 3, 'Cooler Master MasterCase H500', 1, 6, 2),
-(7, 1, 'Corsair Builder Series VS450 80PLUS V2', 2, 4, 2),
-(8, 2, 'Corsair Builder Series VS550 80PLUS V2', 2, 6, 2),
-(9, 3, 'Corsair Builder Series VS650 80PLUS V2', 2, 8, 2),
-(10, 2, 'Corsair RM650x V2 80PLUS Gold', 2, 7, 2),
-(11, 3, 'Seagate BarraCuda 2 To (ST2000DM008)', 3, 4, 2),
-(12, 1, 'Seagate BarraCuda 1 To (ST1000DM010)', 3, 9, 2),
-(13, 3, 'Samsung SSD 860 QVO 1 To', 3, 12, 2),
-(14, 1, 'Samsung SSD 860 EVO 500 Go', 3, 14, 2);
+INSERT INTO `t_produits` (`NumProd`, `NumFour`, `NomProd`, `NumSCat`) VALUES
+(1, 1, 'Zalman i3', 1),
+(2, 2, 'Cooler Master MasterBox Q300L', 1),
+(3, 1, 'Zalman i3', 1),
+(4, 2, 'Cooler Master MasterBox Q300L', 1),
+(5, 2, 'Cooler Master MasterBox E500L Argent', 1),
+(6, 3, 'Cooler Master MasterCase H500', 1),
+(7, 1, 'Corsair Builder Series VS450 80PLUS V2', 2),
+(8, 2, 'Corsair Builder Series VS550 80PLUS V2', 2),
+(9, 3, 'Corsair Builder Series VS650 80PLUS V2', 2),
+(10, 2, 'Corsair RM650x V2 80PLUS Gold', 2),
+(11, 3, 'Seagate BarraCuda 2 To (ST2000DM008)', 3),
+(12, 1, 'Seagate BarraCuda 1 To (ST1000DM010)', 3),
+(13, 3, 'Samsung SSD 860 QVO 1 To', 3),
+(14, 1, 'Samsung SSD 860 EVO 500 Go', 3);
 
 -- --------------------------------------------------------
 
@@ -201,6 +231,16 @@ CREATE TABLE `t_ventesproduits` (
   `TauxTva` int(11) NOT NULL,
   `QteVen` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `t_ventesproduits`
+--
+
+INSERT INTO `t_ventesproduits` (`NumVenProd`, `NumLisFact`, `NumProd`, `NumCli`, `NumAchProd`, `PrixVenUnit`, `TauxTva`, `QteVen`) VALUES
+(1, 1, 5, 5, 1, 100, 21, 1),
+(2, 2, 2, 4, 2, 120, 21, 1),
+(3, 3, 4, 6, 3, 140, 21, 1),
+(4, 4, 6, 3, 4, 150, 21, 1);
 
 --
 -- Index pour les tables déchargées
@@ -266,7 +306,9 @@ ALTER TABLE `t_s_categories`
 ALTER TABLE `t_ventesproduits`
   ADD PRIMARY KEY (`NumVenProd`),
   ADD KEY `NumLisFact` (`NumLisFact`),
-  ADD KEY `NumAchProd` (`NumAchProd`);
+  ADD KEY `NumAchProd` (`NumAchProd`),
+  ADD KEY `NumCli` (`NumCli`),
+  ADD KEY `NumProd` (`NumProd`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
@@ -276,7 +318,7 @@ ALTER TABLE `t_ventesproduits`
 -- AUTO_INCREMENT pour la table `t_achatsproduits`
 --
 ALTER TABLE `t_achatsproduits`
-  MODIFY `NumAchProd` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `NumAchProd` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT pour la table `t_categories`
@@ -288,13 +330,13 @@ ALTER TABLE `t_categories`
 -- AUTO_INCREMENT pour la table `t_clients`
 --
 ALTER TABLE `t_clients`
-  MODIFY `NumCli` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `NumCli` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT pour la table `t_factures`
 --
 ALTER TABLE `t_factures`
-  MODIFY `NumListFact` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `NumListFact` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT pour la table `t_fournisseurs`
@@ -318,7 +360,7 @@ ALTER TABLE `t_s_categories`
 -- AUTO_INCREMENT pour la table `t_ventesproduits`
 --
 ALTER TABLE `t_ventesproduits`
-  MODIFY `NumVenProd` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `NumVenProd` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Contraintes pour les tables déchargées
@@ -354,7 +396,9 @@ ALTER TABLE `t_s_categories`
 --
 ALTER TABLE `t_ventesproduits`
   ADD CONSTRAINT `t_ventesproduits_ibfk_1` FOREIGN KEY (`NumLisFact`) REFERENCES `t_factures` (`NumListFact`),
-  ADD CONSTRAINT `t_ventesproduits_ibfk_2` FOREIGN KEY (`NumAchProd`) REFERENCES `t_achatsproduits` (`NumAchProd`);
+  ADD CONSTRAINT `t_ventesproduits_ibfk_2` FOREIGN KEY (`NumAchProd`) REFERENCES `t_achatsproduits` (`NumAchProd`),
+  ADD CONSTRAINT `t_ventesproduits_ibfk_3` FOREIGN KEY (`NumCli`) REFERENCES `t_clients` (`NumCli`),
+  ADD CONSTRAINT `t_ventesproduits_ibfk_4` FOREIGN KEY (`NumProd`) REFERENCES `t_produits` (`NumProd`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
